bb133779634a3c6b6b5cfba8f355f42d8f63844c
Moses Onerhime <mosesonerhime11@gmail.com>
2024-03-31 16:54:31 +0100

    Add project files.

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "test.h"
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello", "Successful", NULL);
+
+	return 0;
+
+	//test
+
+}
